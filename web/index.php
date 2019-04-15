147645a1e22f546b6563a77b548e6c9ac9f4ec5e
tonycony <46638919+tonycony@users.noreply.github.com>
2019-04-16 00:36:04 +0800

    Update index.php

--- web/index.php
+++ web/index.php
@@ -39,3 +39,93 @@ $post_data = [
 			  ]
 			];
 push($post_data,$access_token);
+
+/*if($type == "text"){
+	
+	$sql="insert into user(user_id) values ('$user_id')";
+	mysqli_query($link,$sql);
+	
+	$sql9 = "SELECT * FROM user where user_id = '$user_id'";
+	$result2 = mysqli_query($link,$sql9);
+	$row = mysqli_fetch_array($result2);
+	
+	if($row['user_name']==NULL)
+	{
+		if(substr($message,0,7)=="姓名@")
+		{
+			$name=substr($message,7);
+			$sql="UPDATE user set user_name='$name' where user_id='$user_id'";
+			mysqli_query($link,$sql);
+			$post_data = [
+			  "replyToken" => $reply_token,
+			  "messages" => [
+				[
+				  "type" => "text",
+				  "text" =>  "你好 $name  "."你可以使用其他功能了"
+				]
+			  ]
+			];
+			push($post_data,$access_token);
+		}
+		else
+		{
+			$post_data = [
+			  "replyToken" => $reply_token,
+			  "messages" => [
+				[
+				  "type" => "text",
+				  //"text" => "你好 $message \n哈哈 $message" ,
+				  "text" =>  "姓名格式輸入錯誤喔 格式為:姓名@xxx"
+				]
+			  ]
+			];
+			push($post_data,$access_token);
+		}
+		
+	}
+	else
+	{
+		switch ($message)
+		{
+			
+			  case "@空氣品質":
+				$sql="SELECT * FROM air_information ORDER BY ID DESC LIMIT 1";//選擇最新的空氣資訊
+				$result=mysqli_query($link,$sql);
+				$row = mysqli_fetch_array($result);
+				$replymessage='現在的溫度是'.(string)$row['Temperature']."°C\n"
+				.'濕度是'.(string)$row['Humidity']."%\n"
+				.'Co濃度是'.(string)$row['Co']."%\n"
+				.'PM2.5是'.(string)$row['PM25'];//回傳給使用者之資訊 \n要用""
+				$post_data = [
+				  "replyToken" => $reply_token,
+				  "messages" => [
+					[
+					  "type" => "text",
+					  "text" => $replymessage
+					]
+				  ]
+				]; 
+				push($post_data,$access_token);
+				break;
+			  case "@關閉提醒":
+				$sql="UPDATE close_reminder set reminder=0 where only=1";//選擇最新的空氣資訊
+				$result=mysqli_query($link,$sql);
+				$replymessage='已關閉提醒5分鐘';//回傳給使用者之資訊 \n要用""
+				$post_data = [
+				  "replyToken" => $reply_token,
+				  "messages" => [
+					[
+					  "type" => "text",
+					  "text" => $replymessage
+					]
+				  ]
+				]; 
+				push($post_data,$access_token);
+				break;
+		} 
+	}
+}
+*/
+
+
+?>
