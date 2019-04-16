cd3699b29083fd14483167b153832503c4208363
tonycony <46638919+tonycony@users.noreply.github.com>
2019-04-16 16:54:53 +0800

    Update index.php

--- web/index.php
+++ web/index.php
@@ -19,7 +19,7 @@ function push($post_data,$access_token)
 	curl_close($ch); 
 }
 
-//include("mysql_connect.inc.php");
+include("mysql_connect.inc.php");
 $access_token ='z8+Cz/5lm0NszzRXsCqI7pFHqfTpG0R1ui9+1qqjQpp6PqEG3NRodAqmy5Ak12bGf1rH2dE461YF4pmW+vH7f6RwWHwwkp5W0Hh6nQfD8aEyzJB+Cgw8MbZVIiDPVuwJ+VFFrA5iUq4a1dw2lq78XQdB04t89/1O/w1cDnyilFU=';
 
 $json_string = file_get_contents('php://input');
@@ -29,145 +29,17 @@ $type  = $event->{"message"}->{"type"};
 $message = $event->{"message"}->{"text"};
 $user_id  = $event->{"source"}->{"userId"};
 $reply_token = $event->{"replyToken"};
-/*$post_data = [
-			  "replyToken" => $reply_token,
-			  "messages" => [
-				[
-				  "type" => "text",
-				  "text" =>  "你好 你可以使用其他功能了"
-				]
-			  ]
-			];*/
-$post_data = [
-			  "replyToken" => $reply_token,
-			  "messages" => [
-				[
-				  "type" => "template",
-				  "altText" => "this is a carousel template",
-				  "template" => [
-					"type" => "carousel",
-					"actions" => [],
-					"columns" => [
-					  [
-						"title" => "Q1",
-						"text" => "請問今天是洗澡，還是泡澡?",
-						"actions" => [
-						  [
-							"type" => "postback",
-							"label" => "泡澡",
-							"text" => "泡澡",
-							"data" => "{Q1}泡澡"
-						  ],
-						  [
-							"type" => "postback",
-							"label" => "沖澡",
-							"text" => "沖澡",
-							"data" => "{Q1}沖澡"
-						  ],
-						  [
-							"type" => "postback",
-							"label" => "擦澡",
-							"text" => "擦澡",
-							"data" => "{Q1}擦澡"
-						  ]
-						]
-					  ],
-					  [
-						"title"=> "Q1",
-						"text"=> "請問今天是洗澡，還是泡澡?",
-						"actions"=> [
-						  [
-							"type" => "postback",
-							"label" => "不想洗澡",
-							"text" => "不想洗澡",
-							"data" => "{Q1}不想洗澡"
-						  ],
-						  [
-							"type" => "postback",
-							"label"=> "因行動不便無法洗澡",
-							"text" => "因行動不便無法洗澡",
-							"data" => "{Q1}因行動不便無法洗澡"
-						  ],
-						  [
-							"type" => "message",
-							"label" => "-",
-							"text" => "-"
-						  ]
-						]
-					  ]
-					]
-				  ]
-				]
-			  ]
-			];			
-/*$post_data =[
-  "type" => "template",
-  "altText" => "this is a carousel template",
-  "template" => [
-    "type" => "carousel",
-    "actions" => [],
-    "columns" => [
-      [
-        "title" => "Q1",
-        "text" => "請問今天是洗澡，還是泡澡?",
-        "actions" => [
-          [
-            "type" => "postback",
-            "label" => "泡澡",
-            "text" => "泡澡",
-            "data" => "{Q1}泡澡"
-          ],
-          [
-            "type" => "postback",
-            "label" => "沖澡",
-            "text" => "沖澡",
-            "data" => "{Q1}沖澡"
-          ],
-          [
-            "type" => "postback",
-            "label" => "擦澡",
-            "text" => "擦澡",
-            "data" => "{Q1}擦澡"
-          ]
-        ]
-      ],
-      [
-        "title"=> "Q1",
-        "text"=> "請問今天是洗澡，還是泡澡?",
-        "actions"=> [
-          [
-            "type" => "postback",
-            "label" => "不想洗澡",
-            "text" => "不想洗澡",
-            "data" => "{Q1}不想洗澡"
-          ],
-          [
-            "type" => "postback",
-            "label"=> "因行動不便無法洗澡",
-            "text" => "因行動不便無法洗澡",
-            "data" => "{Q1}因行動不便無法洗澡"
-          ],
-          [
-            "type" => "message",
-            "label" => "-",
-            "text" => "-"
-          ]
-        ]
-      ]
-    ]
-  ]
-]*/
-push($post_data,$access_token);
 
-/*if($type == "text"){
-	
+$sql="SELECT user_id FROM user WHERE user_id='$user_id'";
+$result= mysqli_query($link,$sql);
+$row = mysqli_fetch_array($result);
+if($row[0]==null)
+{
 	$sql="insert into user(user_id) values ('$user_id')";
 	mysqli_query($link,$sql);
-	
-	$sql9 = "SELECT * FROM user where user_id = '$user_id'";
-	$result2 = mysqli_query($link,$sql9);
+	$sql2 = "SELECT user_name FROM user where user_id = '$user_id'";
+	$result2 = mysqli_query($link,$sql2);
 	$row = mysqli_fetch_array($result2);
-	
 	if($row['user_name']==NULL)
 	{
 		if(substr($message,0,7)=="姓名@")
@@ -202,49 +74,47 @@ push($post_data,$access_token);
 		}
 		
 	}
-	else
-	{
-		switch ($message)
-		{
-			
-			  case "@空氣品質":
-				$sql="SELECT * FROM air_information ORDER BY ID DESC LIMIT 1";//選擇最新的空氣資訊
-				$result=mysqli_query($link,$sql);
-				$row = mysqli_fetch_array($result);
-				$replymessage='現在的溫度是'.(string)$row['Temperature']."°C\n"
-				.'濕度是'.(string)$row['Humidity']."%\n"
-				.'Co濃度是'.(string)$row['Co']."%\n"
-				.'PM2.5是'.(string)$row['PM25'];//回傳給使用者之資訊 \n要用""
-				$post_data = [
-				  "replyToken" => $reply_token,
-				  "messages" => [
-					[
-					  "type" => "text",
-					  "text" => $replymessage
-					]
-				  ]
-				]; 
-				push($post_data,$access_token);
-				break;
-			  case "@關閉提醒":
-				$sql="UPDATE close_reminder set reminder=0 where only=1";//選擇最新的空氣資訊
-				$result=mysqli_query($link,$sql);
-				$replymessage='已關閉提醒5分鐘';//回傳給使用者之資訊 \n要用""
-				$post_data = [
-				  "replyToken" => $reply_token,
-				  "messages" => [
-					[
-					  "type" => "text",
-					  "text" => $replymessage
-					]
-				  ]
-				]; 
-				push($post_data,$access_token);
-				break;
-		} 
-	}
+	
 }
-*/
+$sql2 = "SELECT user_name FROM user where user_id = '$user_id'";
+$result2 = mysqli_query($link,$sql2);
+$row = mysqli_fetch_array($result2);
+	if($row['user_name']==NULL)
+	{
+		if(substr($message,0,7)=="姓名@")
+		{
+			$name=substr($message,7);
+			$sql="UPDATE user set user_name='$name' where user_id='$user_id'";
+			mysqli_query($link,$sql);
+			$post_data = [
+			  "replyToken" => $reply_token,
+			  "messages" => [
+				[
+				  "type" => "text",
+				  "text" =>  "你好 $name  "."你可以使用其他功能了"
+				]
+			  ]
+			];
+			push($post_data,$access_token);
+		}
+		else
+		{
+			$post_data = [
+			  "replyToken" => $reply_token,
+			  "messages" => [
+				[
+				  "type" => "text",
+				  //"text" => "你好 $message \n哈哈 $message" ,
+				  "text" =>  "姓名格式輸入錯誤喔 格式為:姓名@xxx"
+				]
+			  ]
+			];
+			push($post_data,$access_token);
+		}
+		
+	}
+
+
 
 
 ?>
