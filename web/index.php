908832d088195bcc4a729879efa94e70de4b6025
tonycony <46638919+tonycony@users.noreply.github.com>
2019-07-29 16:10:55 +0800

    Update index.php

--- web/index.php
+++ web/index.php
@@ -126,6 +126,83 @@ $question_num=$row["question_num"];
 $user_name=$row["user_name"];
 $date=date("Y.m.d");
 /////////////////////////////////////////////////////////檢查第幾題了
+if($message=="@健康紀錄")
+{
+	<!doctype html>
+<?php
+ $x ="星期一";
+?>
+<html>
+
+<head>
+	<title>每周健康紀錄</title>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js"></script>
+
+</head>
+
+<body>
+  <a id="link"></a>
+	<canvas id="myChart" width="10" height="10" style='width:10px;height:10px'></canvas>
+<script>
+var ctx = document.getElementById('myChart').getContext('2d');
+var y ='<?php echo $x ?>';
+var myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: [y, '星期二', '星期三', '星期四', '星期五', '星期六', '星期日'],
+        datasets: [{
+            label: '收縮壓',
+            data: [20.4, 19, 3, 5, 0, 3,5],
+            backgroundColor:'rgba(255, 99, 132)',  
+            borderColor: 'rgb(255, 99, 132)',
+						fill:false,
+            borderWidth: 2
+        },{
+        		label: '舒張壓',
+            data: [70, 50, 40, 30, 10, 10,50],
+            backgroundColor:'rgba(54, 162, 235)',  
+            borderColor: 'rgb(54, 162, 235)',
+						fill:false,
+            borderWidth: 2
+        
+        	},{
+          	label: '脈搏',
+            data: [70, 58, 49, 93, 91, 81,50],
+            backgroundColor:'rgba(153, 102, 255)',  
+            borderColor: 'rgb(153, 102, 255)',
+						fill:false,
+            borderWidth: 2
+          
+          	}]
+    },
+    options: {
+       animation: {
+          onComplete: function () {
+          var ctx = this.chart.ctx;
+          ctx.font = Chart.helpers.fontString(Chart.defaults.global.defaultFontFamily, 'normal', 						         Chart.defaults.global.defaultFontFamily);
+          ctx.fillStyle = "black";
+          ctx.textAlign = 'center';
+          ctx.textBaseline = 'bottom';
+
+          this.data.datasets.forEach(function (dataset)
+          {
+              for (var i = 0; i < dataset.data.length; i++) {
+                  for(var key in dataset._meta)
+                  {
+                      var model = dataset._meta[key].data[i]._model;
+                      ctx.fillText(dataset.data[i], model.x, model.y - 5);
+                  }
+              }
+          });
+    }
+}
+    }
+});
+</script>
+</body>
+
+</html>
+}
 if($message=="@填寫問卷")//開始填寫問卷
 {
 	$sql = "UPDATE user set question_num=1 where user_id='$user_id'";//題號改為1開始
