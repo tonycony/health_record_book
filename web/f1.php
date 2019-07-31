4c07161d1364b7335a003c6304123d9f4ea86574
tonycony <46638919+tonycony@users.noreply.github.com>
2019-07-31 13:48:33 +0800

    Update f1.php

--- web/f1.php
+++ web/f1.php
@@ -1,4 +1,22 @@
 <?php
+function push($post_data,$access_token)
+{
+	//fwrite($file, json_encode($post_data)."\n");
+	$ch = curl_init("https://api.line.me/v2/bot/message/reply");
+	curl_setopt($ch, CURLOPT_POST, true);
+	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post_data));
+	curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+		'Content-Type: application/json',
+		'Authorization: Bearer '.$access_token
+		//'Authorization: Bearer '. TOKEN
+	));
+	$result = curl_exec($ch);
+	curl_close($ch); 
+}
 function f1($data,$user_id,$link,$access_token)
 {
 	if($data=="[f1]記錄用藥")
