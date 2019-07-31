e2ca1a70f4e08a78ca677563e209d47401310e97
tonycony <46638919+tonycony@users.noreply.github.com>
2019-07-31 10:40:32 +0800

    Update index.php

--- web/index.php
+++ web/index.php
@@ -133,28 +133,28 @@ if($message=="@用藥紀錄")
 		"replyToken" => $reply_token,
 		"messages" => [
 			[
-			  "type": "template",
-			  "altText": "this is a buttons template",
-			  "template": [
-			    "type": "buttons",
-			    "actions": [
+			  "type"=> "template",
+			  "altText"=> "this is a buttons template",
+			  "template"=> [
+			    "type"=> "buttons",
+			    "actions"=> [
 			      [
-				"type": "postback",
-				"label": "記錄用藥",
-				"text": "記錄用藥",
-				"data": "[f1]記錄用藥"
+				"type"=> "postback",
+				"label"=> "記錄用藥",
+				"text"=> "記錄用藥",
+				"data"=> "[f1]記錄用藥"
 			      ],
 			      [
-				"type": "postback",
-				"label": "查詢藥品",
-				"text": "查詢藥品",
-				"data": "[f1]查詢藥品"
+				"type"=> "postback",
+				"label"=> "查詢藥品",
+				"text"=> "查詢藥品",
+				"data"=> "[f1]查詢藥品"
 			      ]
 			    ],
-			    "title": "用藥紀錄",
-			    "text": "選擇功能"
+			    "title"=> "用藥紀錄",
+			    "text"=> "選擇功能"
 			  ]
-			]				
+			]
 				
 		]
 	];
