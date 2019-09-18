32451562bfc0d04ef4c68baa65b49a8d68317465
tonycony <46638919+tonycony@users.noreply.github.com>
2019-09-18 19:26:34 +0800

    Update f1.php

--- web/f1.php
+++ web/f1.php
@@ -49,27 +49,55 @@ function f1($data,$user_id,$link,$access_token,$reply_token,$time)
 		
 		
 	}
-	/*
-	else if($data=="[f02]看診時間提醒")
+	
+	else if($data=="[f03]每日記錄")
 	{
-		$sql = "UPDATE user set function_num=0 where user_id='$user_id'";
-		mysqli_query($link,$sql);
-		$sql2 = "SELECT time FROM user WHERE user_id='$user_id'";
-		$result=mysqli_query($link,$sql2);
-		$row = mysqli_fetch_array($result);
-		$post_data = [
-			"replyToken" => $reply_token,
-			"messages" => [
-				[
-				  "type"=> "text",
-				  "text"=> "提醒你，你的門診時間為: $row[0] \n不要忘記了喔"
-				]
-			]
-		];
+		$post_data = 
+					[
+						"replyToken" => $reply_token,
+						"messages" => [
+							[
+								  "type"=> "template",
+								  "altText"=> "this is a buttons template",
+								  "template"=> [
+									"type"=> "buttons",
+									"actions"=> [
+									  [
+										"type"=> "postback",
+										"label"=> "脈搏",
+										"text"=> "脈搏",
+										"data"=> "[f03]脈搏"
+									  ],
+									  [
+										"type"=> "postback",
+										"label"=> "血壓",
+										"text"=> "血壓",
+										"data"=> "[f03]血壓"
+									  ],
+									  [
+										"type"=> "postback",
+										"label"=> "體重",
+										"text"=> "體重",
+										"data"=> "[f03]體重"
+									  ],
+									  [
+										"type"=> "postback",
+										"label"=> "血糖",
+										"text"=> "血糖",
+										"data"=> "[f03]血糖"
+									  ]
+									],
+									"title"=> "每日記錄",
+									"text"=> "選擇紀錄事項"
+								  ]
+							]
+								
+						]
+					];
 		push($post_data,$access_token);
 		
 	}
-	*/
+	
 	return 0;
 }
 ?>
